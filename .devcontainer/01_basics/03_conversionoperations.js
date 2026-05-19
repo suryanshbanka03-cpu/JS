d64b651bb04f6a523f820a6c986bfcb4781df949
SURYANSH BANKA <suryanshbanka03@gmail.com>
2026-05-19 12:12:57 +0000

    conversionoperations

---
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+//let score = "33abf" = NaN
+//let score = null = 0
+//let score = undefined = NaN
+//let score="sury"= NaN
+//let score= true= 1
+let score =33
+console.log(typeof score)
+let n = Number(score)
+console.log(typeof n)
+console.log(n)
+let a =1
+let boolean = Boolean(a)
+console.log(boolean)
+//0 = false
+//"suryansh" = true
+let num= 33
+let snum= String(num)
+console.log(snum)
+console.log(typeof snum)
