5fc1cf5b3df7dca2ba574aa61fb3b18cb1add8fe
SURYANSH BANKA <suryanshbanka03@gmail.com>
2026-05-19 12:28:57 +0000

    conversionoperations

--- .devcontainer/01_basics/03_conversionoperations.js
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -4,16 +4,37 @@
 //let score="sury"= NaN
 //let score= true= 1
 let score =33
-console.log(typeof score)
+//console.log(typeof score)
 let n = Number(score)
-console.log(typeof n)
-console.log(n)
+//console.log(typeof n)
+//console.log(n)
 let a =1
 let boolean = Boolean(a)
-console.log(boolean)
+//console.log(boolean)
 //0 = false
 //"suryansh" = true
 let num= 33
 let snum= String(num)
-console.log(snum)
-console.log(typeof snum)
+//console.log(snum)
+//console.log(typeof snum)
+
+// *******************operations*******************
+
+//+,-,*,/,%
+let s= "hello"
+let s1= "sury"
+let s2= s+s1;
+
+//console.log(s2)
+//console.log("1"+ 2)
+//console.log(1+ "2")
+//console.log("1"+"2")
+//console.log(1+2+"3") // all treated as numbers then string
+//console.log("1"+2+2) // all will be treated as strings
+
+let num1=num2=num3= 2+2
+
+let g=100
+//g++
+++g
+console.log(g)
